a8991bec519c539243019987c73f4e329652c8ff
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-10-21 18:19:20 +0900

    https://djangogirlsjapan.gitbooks.io/workshop_tutorialjp/django_templates/まで進行

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # __lte: less than equal
+    # 投稿日時が現在時刻以下の投稿を抽出
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
